86e1092827a7fb3771da78fc961c53828ce141d1
Iván L. Murillo <diskman199@gmail.com>
2022-08-14 00:04:36 -0500

    Added sensor support with TFT screen
    Just for testing purposes

--- tamagotchi-plant-esp32-sensors.ino
+++ tamagotchi-plant-esp32-sensors.ino
@@ -2,15 +2,18 @@
 #include <DHTStable.h>
 #include <BH1750.h>
 #include <Wire.h>
+#include <SPI.h>
+#include <TFT_eSPI.h>
 
 // Pin declarations
 const byte TEST_LED = 2;
-const byte DHT22_PIN = 15;
+const byte DHT22_PIN = 27;
 const byte SOIL_MOISTURE_PIN = 13;
 
 // Object declarations
 DHTStable DHT;
 BH1750 lightMeter;
+TFT_eSPI tft = TFT_eSPI(); 
 
 void takeMeasurement(){
   DHT.read22(DHT22_PIN);
@@ -20,7 +23,20 @@ void takeMeasurement(){
   int soilRead = analogRead(SOIL_MOISTURE_PIN);
   float soilMoisture = map(soilRead, 0, 4095, 100, 0);
   Serial.println("Humedad: " + String(soilMoisture) + "%\t\t" + String(soilRead));
-  //Serial.println("Temperatura: " + String(temp) + "°C\t\tHumedad: " + String(hum) + "%\t\tLuminosidad: " + String(light) + "lx");
+  Serial.println("Temperatura: " + String(temp) + "°C\t\tHumedad: " + String(hum) + "%\t\tLuminosidad: " + String(light) + "lx");
+  
+  tft.fillScreen(TFT_BLACK);
+  tft.setCursor(0, 40);
+  tft.setTextColor(TFT_ORANGE, TFT_BLACK);
+  tft.println("Temperatura: " + String(temp) + "°C");
+  tft.setTextColor(TFT_CYAN, TFT_BLACK);
+  tft.println("Humedad aire: " + String(hum) + "%");
+  
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.println("Luminosidad: " + String(light) + "lx");
+  
+  tft.setTextColor(TFT_BLUE, TFT_BLACK);
+  tft.println("Humedad tierra: " + String(soilMoisture) + "%");
 }
 
 void setup() {
@@ -43,6 +59,17 @@ void setup() {
     digitalWrite(TEST_LED, LOW);
     delay(500);
   }
+
+  //Init screen
+  tft.init();
+  tft.fillScreen(TFT_BLACK);
+  tft.setFreeFont(&FreeSans9pt7b);
+  // Set "cursor" at top left corner of display (0,0) and select font 4
+  tft.setCursor(0, 40);
+  // Set the font colour to be white with a black background
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  // We can now plot text on screen using the "print" class
+  tft.println("Hola mundo :)\n");
 }
 
 void loop() {

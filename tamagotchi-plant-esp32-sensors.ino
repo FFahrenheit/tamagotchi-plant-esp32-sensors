4f472096004ab55fa12da0319e73dd7b13e4e390
Iván L. Murillo <diskman199@gmail.com>
2022-09-10 21:38:22 -0500

    Added WiFi connection and HTTP requests

--- tamagotchi-plant-esp32-sensors.ino
+++ tamagotchi-plant-esp32-sensors.ino
@@ -1,9 +1,17 @@
 //Libraries
+#include <Arduino_JSON.h>
 #include <DHTStable.h>
 #include <BH1750.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <TFT_eSPI.h>
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <HTTPClient.h>
+
+//Client config
+String MY_PRIVATE_ID = "1";
+String SERVER_ADDRESS = "http://64.227.14.152:3000/api/planta/" + MY_PRIVATE_ID;
 
 // Pin declarations
 const byte TEST_LED = 2;
@@ -15,6 +23,9 @@ hw_timer_t * timer = NULL;      // Hardware timer
 DHTStable DHT;
 BH1750 lightMeter;
 TFT_eSPI tft = TFT_eSPI();
+WiFiMulti wifiMulti;
+WiFiClient wifiClient;
+
 
 //Global variables
 float hum;
@@ -22,6 +33,14 @@ float temp;
 float light;
 float soilMoisture;
 bool needsToMeasure = false;
+
+//Set points
+double max_hum = 0.0f, min_hum = 0.0f, max_temp = 0.0f, min_temp = 0.0f;
+
+char *redes[2][2] = {
+  {"TheCoolestWiFiLM", "LopezMurillo128"},
+  {"WifiLM", "LopezMurillo128"}
+};
 
 void takeMeasurement(){
   DHT.read22(DHT22_PIN);
@@ -45,6 +64,8 @@ void takeMeasurement(){
   
   tft.setTextColor(TFT_BLUE, TFT_BLACK);
   tft.println("Humedad tierra: " + String(soilMoisture) + "%");
+
+  sendData();
 }
 
 void IRAM_ATTR onTimer(){
@@ -88,6 +109,34 @@ void setup() {
   timerAttachInterrupt(timer, &onTimer, true);
   timerAlarmWrite(timer, 2500000, true);
   timerAlarmEnable(timer);
+
+  //WiFi Init
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  for(auto red : redes){
+    Serial.print("SSID: ");
+    Serial.print(red[0]);
+    Serial.print("\t\tPassword: ");
+    Serial.println(red[1]);
+
+    wifiMulti.addAP(red[0], red[1]);
+  }
+
+  Serial.println("Connecting to WiFi...");
+  int i = 0;
+
+  while (wifiMulti.run() != WL_CONNECTED) {
+    delay(1250);
+    Serial.print(++i); Serial.print(' ');
+  }
+  
+  Serial.println('\n');
+  Serial.print("Connected to ");
+  Serial.println(WiFi.SSID());              
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());
+
+  setpointsConfig();
 }
 
 void loop() {
@@ -95,4 +144,71 @@ void loop() {
     takeMeasurement();
     needsToMeasure = false;
   }
+}
+
+void sendData(){
+  HTTPClient http;
+  String body = "temperatura=" + String(temp) + "&humedad_ambiente=" + String(hum) + "&luminosidad=" + String(light) + "&humedad_tierra=" + String(soilMoisture);
+  Serial.println(body); 
+
+  http.begin(wifiClient, SERVER_ADDRESS);  //Specify request destination
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  
+  int httpCode = http.PUT(body);               //Send the request
+  Serial.println("Response: " + String(httpCode));
+
+
+  if (httpCode > 0) { //Check the returning code
+ 
+    String payload = http.getString();   //Get the request response payload
+    JSONVar response = JSON.parse(payload);
+
+    if (JSON.typeof(response) != "undefined" && response.hasOwnProperty("title"))
+    {
+      Serial.println(response["title"]);
+    }
+    else
+    {
+      Serial.println(response);
+    }
+  }
+ 
+  http.end();   //Close connection
+}
+
+void setpointsConfig(){
+  HTTPClient http;
+  http.begin(wifiClient, SERVER_ADDRESS + "/config");  //Specify request destination
+  int httpCode;
+  
+  do{
+    httpCode = http.GET();                  //Send the request
+    Serial.println("Response: " + String(httpCode));
+
+  if (httpCode > 0) { //Check the returning code
+ 
+    String payload = http.getString();   //Get the request response payload
+    JSONVar response = JSON.parse(payload);
+
+    if (JSON.typeof(response) != "undefined" && response.hasOwnProperty("max_temp"))
+    {
+      max_temp = (double) response["max_temp"];
+      min_temp = (double) response["min_temp"];
+      max_hum = (double) response["max_hum"];
+      min_hum = (double) response["min_hum"];
+
+      Serial.println("Maxima temperatura: " + String(max_temp));
+      Serial.println("Minima temperatura: " + String(min_temp));
+      Serial.println("Maxima humedad: " + String(max_hum));
+      Serial.println("Minima humedad: " + String(min_hum));
+    }
+    else
+    {
+      Serial.println(response);
+    }
+  }
+ 
+  }while(httpCode < 0);
+  
+  http.end();   //Close connection
 }

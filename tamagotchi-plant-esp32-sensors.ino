7e911572dc858cf6ba95ca05e1d9563d43b0a868
Iván L. Murillo <diskman199@gmail.com>
2022-09-25 16:22:33 -0500

    Dashboard animations

--- tamagotchi-plant-esp32-sensors.ino
+++ tamagotchi-plant-esp32-sensors.ino
@@ -9,6 +9,14 @@
 #include <WiFiMulti.h>
 #include <HTTPClient.h>
 
+#include "assets/ok.h";
+#include "assets/dashboard.h";
+#include "assets/font.h"
+
+void sendData();
+void setpointsConfig();
+void drawDashboard();
+
 //Client config
 String MY_PRIVATE_ID = "1";
 String SERVER_ADDRESS = "http://64.227.14.152:3000/api/planta/" + MY_PRIVATE_ID;
@@ -16,7 +24,7 @@ String SERVER_ADDRESS = "http://64.227.14.152:3000/api/planta/" + MY_PRIVATE_ID;
 // Pin declarations
 const byte TEST_LED = 2;
 const byte DHT22_PIN = 27;
-const byte SOIL_MOISTURE_PIN = 13;
+const byte SOIL_MOISTURE_PIN = 33;
 
 // Object declarations
 hw_timer_t * timer = NULL;      // Hardware timer
@@ -43,28 +51,16 @@ char *redes[2][2] = {
 };
 
 void takeMeasurement(){
+  int soilRead = analogRead(SOIL_MOISTURE_PIN);
   DHT.read22(DHT22_PIN);
   hum = DHT.getHumidity();
   temp = DHT.getTemperature();
   light = lightMeter.readLightLevel();
-  int soilRead = analogRead(SOIL_MOISTURE_PIN);
   soilMoisture = map(soilRead, 0, 4095, 100, 0);
   Serial.println("Humedad: " + String(soilMoisture) + "%\t\t" + String(soilRead));
   Serial.println("Temperatura: " + String(temp) + "°C\t\tHumedad: " + String(hum) + "%\t\tLuminosidad: " + String(light) + "lx");
-  
-  tft.fillScreen(TFT_BLACK);
-  tft.setCursor(0, 40);
-  tft.setTextColor(TFT_ORANGE, TFT_BLACK);
-  tft.println("Temperatura: " + String(temp) + "°C");
-  tft.setTextColor(TFT_CYAN, TFT_BLACK);
-  tft.println("Humedad aire: " + String(hum) + "%");
-  
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.println("Luminosidad: " + String(light) + "lx");
-  
-  tft.setTextColor(TFT_BLUE, TFT_BLACK);
-  tft.println("Humedad tierra: " + String(soilMoisture) + "%");
 
+  drawDashboard();
   sendData();
 }
 
@@ -74,6 +70,9 @@ void IRAM_ATTR onTimer(){
 
 void setup() {
   // Pin modes
+  
+  pinMode(SOIL_MOISTURE_PIN, INPUT);
+  
   pinMode(TEST_LED, OUTPUT);
   digitalWrite(TEST_LED, LOW);
   
@@ -83,20 +82,27 @@ void setup() {
 
   //Init screen
   tft.init();
+  tft.setSwapBytes(true);
   tft.fillScreen(TFT_BLACK);
-  tft.setFreeFont(&FreeSans9pt7b);
+  tft.setFreeFont(&Arimo_Regular_24);
   // Set "cursor" at top left corner of display (0,0) and select font 4
   tft.setCursor(0, 40);
   // Set the font colour to be white with a black background
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
   // We can now plot text on screen using the "print" class
-  tft.println("Hola mundo :)\n");
-  tft.println("Inicializando...");
+  
+  //tft.println("Hola mundo :)\n");
+  //tft.println("Inicializando...");
+
+  tft.pushImage(OK_OFFSET_X, OK_OFFSET_Y, OK_WIDTH, OK_HEIGHT, OK_SPRITE);
+  //drawDashboard();
+  delay(2500);
 
   // Init I2C
-  Wire.begin();
+  Wire.begin(15, 5);
   //Init light meter
-  while(!lightMeter.begin()){
+  
+  while(! lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE_2, 0x23, &Wire) ){
     Serial.println("Error on BH1750 init...");
     digitalWrite(TEST_LED, HIGH);
     delay(500);
@@ -211,4 +217,28 @@ void setpointsConfig(){
   }while(httpCode < 0);
   
   http.end();   //Close connection
+}
+
+void drawDashboard(){
+  tft.setSwapBytes(true);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.fillScreen(TFT_BLACK);
+  
+  tft.pushImage(TEMP_OFFSET_X, TEMP_OFFSET_Y, TEMP_WIDTH, TEMP_HEIGHT, TEMP_SPRITE);
+  tft.setCursor(20, 90);
+  tft.print(String(temp) + "°C");
+  
+  tft.pushImage(SUN_OFFSET_X, SUN_OFFSET_Y, SUN_WIDTH, SUN_HEIGHT, SUN_SPRITE);
+  String luminosidad = String(light);
+  int offsetX = (luminosidad.length() - 3) * 10;
+  tft.setCursor(160 - offsetX, 90);
+  tft.print(luminosidad + "lx");
+  
+  tft.pushImage(HUM_OFFSET_X, HUM_OFFSET_Y, HUM_WIDTH, HUM_HEIGHT, HUM_SPRITE);
+  tft.setCursor(20, 210);
+  tft.print(String(hum) + "%");
+  
+  tft.pushImage(HUMT_OFFSET_X, HUMT_OFFSET_Y, HUMT_WIDTH, HUMT_HEIGHT, HUMT_SPRITE);
+  tft.setCursor(140, 210);
+  tft.print(String(soilMoisture) + "%");
 }
